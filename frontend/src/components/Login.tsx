26471bc95cac19ecfa51e3a89b2a337a0c419436
cseek11 <seeksfx@gmail.com>
2025-08-19 01:20:28 -0400

    update

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { login } from '../api';
+import { login } from '../lib/api';
 
 export function Login() {
   const [email, setEmail] = useState('');
