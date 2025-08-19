a6bd18fa2e4d3cb73faa66cbafe4484011d055ce
cseek11 <seeksfx@gmail.com>
2025-08-19 13:07:14 -0400

    update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './routes/App';
 import './index.css';
+import './custom-calendar.css';
 
 
 const queryClient = new QueryClient();
