086254a0ccf94673fa47a2dc8f65ce607e75f710
cseek11 <seeksfx@gmail.com>
2025-08-19 23:56:49 -0400

    update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx,js,jsx}",
+    "./src/ui-dashboard/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: { extend: {} },
   plugins: [],
 };
