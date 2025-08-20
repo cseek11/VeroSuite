b5300dc28a7e62b45f780f256cf5199502bcb9e0
cseek11 <seeksfx@gmail.com>
2025-08-20 14:48:41 -0400

    update

--- frontend/frontend/src/ui/Spinner.tsx
+++
@@ -1,7 +0,0 @@
-export function Spinner() {
-  return (
-    <div className="w-full h-full flex items-center justify-center p-8">
-      <div className="animate-spin h-8 w-8 rounded-full border-2 border-gray-300 border-t-gray-600" />
-    </div>
-  );
-}
